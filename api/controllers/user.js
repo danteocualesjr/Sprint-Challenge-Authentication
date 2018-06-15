979f50b66f0c42a514ce7a05f390f5ee7da060ba
Dante O. Cuales, Jr <danteocualesjr@gmail.com>
2018-06-16 02:53:26 +0800

    Configured user.js

--- api/controllers/user.js
+++ api/controllers/user.js
@@ -1,10 +1,24 @@
 const User = require('../models/userModels');
 const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
+const { mysecret } = require('../../config');
 
 const createUser = (req, res) => {
   const { username, password } = req.body;
   // create user takes in the username and password and saves a user.
   // our pre save hook should kick in here saving this user to the DB with an encrypted password.
+  User.create({ username, password })
+    .then(({username}) => {
+      const payload = {
+        username
+      };
+      const token = jwt.sign(payload, mysecret);
+      res.status(201).json({ username, token });
+    })
+    .catch(err => {
+      console.log('Error!');
+      res.status(500).json({ error: 'Could not register user into server', message: err.message });
+    });
 };
 
 module.exports = {
